2d3334e5cccd91638e2fa7544264457b8dd79a41
Nathan Che <nanache21@gmail.com>
2026-02-24 18:01:43 -0800

    Added auto-restart and memory limit for docker compose

--- src/vss-engine/src/via_server.py
+++ src/vss-engine/src/via_server.py
@@ -1421,7 +1421,7 @@ class ViaServer:
         # Add CORS middleware to allow frontend access
         self._app.add_middleware(
             CORSMiddleware,
-            allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://0.0.0.0:3000"],
+            allow_origins=["*"],  # Allow all origins for easier deployment
             allow_credentials=True,
             allow_methods=["*"],
             allow_headers=["*"],
